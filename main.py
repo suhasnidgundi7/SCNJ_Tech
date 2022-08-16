c7078352520d4a7724a8f0d6c2a4cf180ea18bb4
suhasnidgundi <suhasnidgundi@gmail.com>
2022-08-16 15:29:43 +0530

    Changes Done To Deploy!

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-from fastapi import FastAPI
-from router import routingapp
-from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
-
-app = FastAPI()
-
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-
-
-app.include_router(routingapp)
-
-
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
